462f1c82244f07268dcc72e4a271358c3b0aa3e9
will-liu <will-liu@live.com>
2022-06-02 00:07:07 +0800

    new wordpress

--- wp-content/saturnwp/frontpage/slider-customslide.php
+++
@@ -1,6 +0,0 @@
-<?php global $saturnwp_a13; ?>
-<?php if(!empty($saturnwp_a13->get_option('customslide')) || is_customize_preview() ) { ?>
-<div class="slidercus">
-<?php echo do_shortcode($saturnwp_a13->get_option('customslide')); ?>
-</div> 
-<?php } ?>
